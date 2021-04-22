ee95715428e37d8c0b8ed7be0f26b96aee82d9d5
Nick Kalpakis <nicholas.kalpakis@dcsg.com>
2021-04-22 07:43:21 -0400

    Header.

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import { Fragment } from 'react';
+import classes from './Header.module.css';
+import headerImg from '../../assets/images/header-img-large.jpeg';
+import HeaderCartButton from './HeaderCartButton';
+const Header = (props) => {
+
+	return (
+		<Fragment>
+			<header className={classes.header}>
+				<h1>Nicks Restarant</h1>
+				<HeaderCartButton/>
+			</header>
+			<div className={classes['main-image']}>
+				<img src={headerImg} alt="food"/>
+			</div>
+		</Fragment>
+	)
+}
+
+export default Header;
